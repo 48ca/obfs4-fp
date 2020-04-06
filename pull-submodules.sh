6d95115f2bca470c99e9bb2f798d82167dc8ac00
James Houghton <jhoughton@virginia.edu>
2020-04-06 00:32:35 -0400

    Add pull submodules script

--- pull-submodules.sh
+++ pull-submodules.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -xe
 
 git submodule update --init --recursive --remote
+
+# If that doesn't work:
+#
+# rmdir obfs4
+# git clone git@github.com:jamesthoughton/obfs4
+# cd fpgen/crawl
+# rmdir oniontree
+# git clone git@github.com:onionltd/oniontree
