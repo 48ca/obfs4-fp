d1e910c295a9fc28903f2b0780302aba28f5a51e
James Houghton <jhoughton@virginia.edu>
2020-05-04 18:00:37 -0400

    Add more classification information

---
+++ classification/classify-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+./good-classify.sh iat0
+./good-classify.sh iat1
+./good-classify.sh iat2
+./good-classify.sh iat3
+./good-classify.sh iat3-5
+
+./good-classify.sh iat0-40
+./good-classify.sh iat2-40
+./good-classify.sh iat3-40
--- classification/good-classify.sh
+++ classification/good-classify.sh
@@ -4,18 +4,33 @@ FP="$1"
 
 ACC="0"
 ITERS=20
+SQRTITERS="$(echo "scale=10;$ITERS" | bc)"
+
+total="0"
+numbers=""
 
 for i in `seq $ITERS`
 do
     a="$(./classify.py $FP | grep "Accuracy" | cut -d' ' -f2)"
+    total=$(echo "scale=10;$total + $a "| bc)
+    numbers="$numbers $a"
     if [[ $a > $ACC ]]
     then
         ACC=$a
-        echo "Updated accuracy to $ACC"
-        set -x
         cp models/$FP-randomforest.sav auto-good-models/$FP-randomforest.sav
-        set +x
     fi
 done
 
-echo "Final accuracy: $ACC"
+echo "$FP"
+echo "Max accuracy: $ACC"
+
+echo "Total: $total"
+avg=$(echo "scale=10;$total / $ITERS" | bc)
+echo "Average accuracy: $avg"
+
+standardDeviation=$(
+    echo $numbers | sed 's/ /\n/g' |
+        awk '{sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/NR - (sum/NR)**2)}'
+)
+echo "Std dev: $standardDeviation"
+echo "Error: $(echo "scale=10;$standardDeviation/$SQRTITERS" | bc)"
